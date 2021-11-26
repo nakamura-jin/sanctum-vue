efff77648c6786f5ee519e9c350259e07f2c901d
中村　迅 <shu.kyu0224@icloud.com>
2021-11-26 14:09:31 +0900

    add js-cookie edit Csrf.js

--- src/apis/Csrf.js
+++ src/apis/Csrf.js
@@ -1,7 +1,16 @@
 import Api from './Api';
+import Cookie from 'js-cookie';
 
 export default {
   getCookie() {
-    return Api.get('/csrf-cookie');
+    let token = Cookie.get("XSRF-TOKEN");
+
+    if (token) {
+      return new Promise(resolve => {
+        resolve(token);
+      });
+    }
+
+    return Api.get("/csrf-cookie");
   }
 }
